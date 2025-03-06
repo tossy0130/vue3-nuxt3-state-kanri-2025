7070e178a73bee585e0d7225cc7defb2d92c761a
tossy0130 <tokotoko33ok@gmail.com>
2025-03-06 23:01:47 +0900

    pinia ストア、ゲッター、アクション

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,26 @@
-<script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+<script setup>
+
+import { useCounterStore } from './stores/counter.ts'
+import CounterComp from './components/CounterComp.vue'
+
+const counterStore = useCounterStore()
+console.log(counterStore.count)
 </script>
 
+
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+  <h1>Pinia 呼び出し</h1>
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
+  <button @click="counterStore.count++">1</button>
 
-  <main>
-    <TheWelcome />
-  </main>
+  <button @click="counterStore.increment">3</button>
+
+  <p>{{ counterStore.count }}</p>
+  <p>{{ counterStore.doublecount }}</p>
+  <p>{{ counterStore.count2 }}</p>
+
+
+  <!-- components/CounterComp.vue  -->
+   <CounterComp />
 </template>
-
-<style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
-</style>
+./stores/counter.ts
